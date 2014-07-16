feb4a7a588df39c9c52b61de82e1cb85c4569fe2
dlasserre <dlasserre@talend.com>
2014-07-17 00:06:57 +0200

    Ajout du parametre dans le constructeur server

--- src/server/server.php
+++ src/server/server.php
@@ -2,7 +2,9 @@
 /** Namespace engine / server */
 namespace engine\server;
 use engine\exception\client;
+use hook\smtp;
 use protocol\definition;
+use protocol\interfaces\hook;
 use protocol\smtp\interpret;
 
 /**
@@ -25,10 +27,11 @@ class server extends initialize
      * @author Damien Lasserre <damien.lasserre@gmail.com>
      * @param definition | interpret $definition
      * @param string $_name
+     * @param hook $_hook
      * @throws \engine\exception\server
      * @throws \engine\exception\client
      */
-    public function __construct(definition $definition, $_name)
+    public function __construct(definition $definition, $_name, hook $_hook = null)
     {
         if(false !== parent::__construct($definition, $_name)) {
             /** protocol name */
@@ -44,14 +47,13 @@ class server extends initialize
                     if(in_array(parent::$_socket, $_streams)) {
                         /** @var resource $_client */
                         $_client = socket_accept(parent::$_socket);
-
                         if($_client > 0) {
-
                             self::$_clients[] = $_client;
                             if($_client > 0) {
-                                $definition->transmission(new socket($_client));
+                                $definition->transmission(new socket($_client, $_hook));
                             }
-                        }
+                        } else /** Exception */
+                            throw new \engine\exception\server(socket_last_error($_client));
                     }
                 }
             }
@@ -65,7 +67,6 @@ class server extends initialize
     {
         /** Require since PHP 4.3.0 */
         declare(ticks = 1);
-
         /** Close socket */
         parent::close();
         exit($sig);
