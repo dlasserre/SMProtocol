91e671d95dde9e46b8bb263fe07b65325d913fcf
dlasserre <dlasserre@talend.com>
2014-07-17 17:18:31 +0200

    Restructuration du select ainsi que la gestion des socket et des object sender, reorganisation des methode hook (dispatch/etc...)

--- src/server/server.php
+++ src/server/server.php
@@ -22,35 +22,73 @@ class server extends initialize
 
     /**
      * @author Damien Lasserre <damien.lasserre@gmail.com>
-     * @param definition | interpret $definition
+     * @param definition $definition
      * @param string $_name
-     * @param hook $_hook
      * @throws \engine\exception\server
      * @throws \engine\exception\client
      */
-    public function __construct(definition $definition, $_name, hook $_hook = null)
+    public function __construct(definition $definition, $_name)
     {
+        pcntl_signal(SIGINT, array($this, 'kill'));
+
         if(false !== parent::__construct($definition, $_name)) {
             /** protocol name */
             $this->_name = $_name;
             /** @var array $_streams */
             $this->_streams = array(parent::$_socket);
+            /** @var array $_hooks */
+            $_hooks = array();
 
-            for(;;) {
+            for(;;)
+            {
                 /** @var array $_streams */
                 $_streams = array_merge((array)self::$_clients, $this->_streams);
 
-                if(@socket_select($_streams, $array = null, $expect = null, null) > 0) {
+                if(@socket_select($_streams, $_write = array(), $expect = null, null) < 1) {
+                    continue;
+                }
+
+                foreach($_streams as $socket) {
                     if(in_array(parent::$_socket, $_streams)) {
-                        /** @var resource $_client */
-                        $_client = socket_accept(parent::$_socket);
-                        if($_client > 0) {
-                            self::$_clients[] = $_client;
+                        /** New connection */
+                        if($socket === parent::$_socket) {
+                            /** @var resource $_client */
+                            $_client = socket_accept(parent::$_socket);
+
                             if($_client > 0) {
-                                $definition->transmission(new socket($_client, $_hook));
+                                self::$_clients[] = $_client;
+                                /** @var string $_hook_class */
+                                $_hook_class = $this->_name.'\hook';
+                                /** @var hook $_hook */
+                                $_hook = new $_hook_class(new sender($_client, $definition));
+                                socket_getpeername($_client, $address, $port);
+                                /** Call preDispatching hook method */
+                                $_hook->preDispatch($address, $port);
+                                $_hooks[(string)$_client] = $_hook;
+
+                                if($_client <= 0) { /** Exception */
+                                    throw new \engine\exception\server(socket_last_error($_client));
+                                }
+                            } else /** Exception */
+                                throw new \engine\exception\server(socket_last_error($_client));
+                        }
+                    } else {
+                        /** @var \hook[] $_hooks */
+                        if(!$_hooks[(string)$socket]->isClosed()) {
+                            /** @var string $_data */
+                            $_data = socket_read($socket, (int)$definition->block_size);
+
+                            if($_data) {
+                                /** @var \hook[] $_hooks */
+                                $_hooks[(string)$socket]->dispatch($_data);
+                                if($_hooks[(string)$socket]->isClosed()) {
+                                    /** Call postDispatching hook method */
+                                    $_hooks[(string)$socket]->postDispatch(null);
+                                    /** remove from client stack */
+                                    unset(self::$_clients[(string)$socket]);
+                                }
                             }
-                        } else /** Exception */
-                            throw new \engine\exception\server(socket_last_error($_client));
+                        }
                     }
                 }
             }
